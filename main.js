478fc01eab4fb979d3220b7e4c816cab6a87c356
SimpleNX <simplepanigrahi17@gmail.com>
2024-08-25 16:35:44 +0530

    Edit main.js to render books if not rendered before

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ let dialog = document.querySelector("#add-prompt");
 let addConf = document.querySelector("#add-co-btn");
 let books = document.querySelector("#books");
 let form = document.querySelector("#add-book-form");
-let bookItem = document.querySelector("#book-grid");
 
 //details for the books
 let author, title, pages, readn;
@@ -15,45 +14,37 @@ function Book(author, title, pages, read, index){
     this.pages = pages;
     this.read = read;
     this.index = index;
+    this.domE = null;
 }
 
-// function clearPrev(){
-//     if(bookItem !== null)
-//         books.clearItem(bookItem);
-//     bookItem = document.createElement("div");
-//     books.appendChild(bookItem);
-// }
-
 function renderBook(){
-    // clearPrev();
     for(const book of bookList){
-        const bookDom = document.createElement("div");
-        let authorP = document.createElement("p");
-        let titleP = document.createElement("p");
-        let pagesP = document.createElement("p");
-        let readP = document.createElement("p");
-        let deleteB = document.createElement("button");
-
-        authorP.innerText = `Author Name : ${book.author}`;
-        titleP.innerText = `Title of the Book : ${book.title}`;
-        pagesP.innerText = `Pages Read : ${book.pages}`;
-        readP.innerText = `Read or not : ${book.read}`;
-        deleteB.innerText = "Delete";
-
-        deleteB.addEventListener("click", ()=>{
-            bookList.splice(book.index, 1);
-            // let bookDelete = document.querySelector(`#books:nth-child(${book.index+1})`);
-            // clearPrev();
-            renderBook();
-        });
-        bookDom.setAttribute("class", "book");
-        bookDom.appendChild(authorP);
-        bookDom.appendChild(titleP);
-        bookDom.appendChild(pagesP);
-        bookDom.appendChild(readP);
-        bookDom.appendChild(deleteB);
-
-        books.appendChild(bookDom);
+        if(book.domE === null){
+            const bookDom = document.createElement("div");
+            let authorP = document.createElement("p");
+            let titleP = document.createElement("p");
+            let pagesP = document.createElement("p");
+            let readP = document.createElement("p");
+            let deleteB = document.createElement("button");
+            authorP.innerText = `Author Name : ${book.author}`;
+            titleP.innerText = `Title of the Book : ${book.title}`;
+            pagesP.innerText = `Pages Read : ${book.pages}`;
+            readP.innerText = `Read or not : ${book.read}`;
+            deleteB.innerText = "Delete";
+            deleteB.addEventListener("click", ()=>{
+                bookList.splice(book.index, 1);
+                books.removeChild(book.domE);
+                renderBook();
+            });
+            bookDom.setAttribute("class", "book");
+            bookDom.appendChild(authorP);
+            bookDom.appendChild(titleP);
+            bookDom.appendChild(pagesP);
+            bookDom.appendChild(readP);
+            bookDom.appendChild(deleteB);
+            book.domE = bookDom;
+            books.appendChild(book.domE);
+        }
     }
 }
 
